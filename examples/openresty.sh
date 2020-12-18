7868657291dd33bd924281bfb5b6d81e15651e04
Leon <leondevlifelog@gmail.com>
2020-12-18 11:29:26 +0800

    Update openresty.sh
    启用 ssl 模块

--- examples/openresty.sh
+++ examples/openresty.sh
@@ -18,7 +18,7 @@ sed -i "s/linux-mips64/linux64-mips64/g" openssl-1.1.1h/config
 
 # compile it
 cd openresty-1.17.8.2
-time ./configure --prefix=/opt/openresty --with-http_v2_module --with-http_auth_request_module --with-pcre=$PWD/../pcre-8.44 --with-openssl=$PWD/../openssl-1.1.1h --with-zlib=$PWD/../zlib-1.2.11  --add-module=$PWD/../ngx-fancyindex-0.4.2
+time ./configure --prefix=/opt/openresty --with-http_v2_module --with-http_ssl_module --with-http_auth_request_module --with-pcre=$PWD/../pcre-8.44 --with-openssl=$PWD/../openssl-1.1.1h --with-zlib=$PWD/../zlib-1.2.11  --add-module=$PWD/../ngx-fancyindex-0.4.2
 time make -j 8
 make install
 
