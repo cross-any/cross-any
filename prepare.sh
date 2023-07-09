6a66bedbd97fc92b2747bd319c150278e368686b
wind <liyong@alot.pw>
2023-07-09 20:06:12 +0800

    [push]upgrade gentoo to latest,add riscv64 and loongarch64

--- prepare.sh
+++ prepare.sh
@@ -21,7 +21,7 @@ emerge -j$JOBS -vn --autounmask-continue --autounmask=y --autounmask-write dev-u
 #emerge -j$JOBS -vn --autounmask-continue --autounmask=y --autounmask-write '=sys-devel/gcc-13*'
 #gcc-config 2
 emerge -j$JOBS -vn --autounmask-continue --autounmask=y --autounmask-write crossdev vim dev-vcs/git app-portage/gentoolkit dev-util/pkgdev sudo file app-admin/eselect
-USE="static-user" QEMU_USER_TARGETS="aarch64 aarch64_be alpha arm armeb cris hppa i386 m68k microblaze microblazeel mips mips64 mips64el mipsel mipsn32 mipsn32el nios2 or1k ppc ppc64 ppc64abi32 ppc64le riscv32 riscv64 s390x sh4 sh4eb sparc sparc32plus sparc64 tilegx xtensa xtensaeb"  emerge   --autounmask-continue --autounmask=y --autounmask-write  -vn -j$JOBS qemu
+USE="static-user" QEMU_USER_TARGETS="loongarch64 hexagon aarch64 aarch64_be alpha arm armeb cris hppa i386 m68k microblaze microblazeel mips mips64 mips64el mipsel mipsn32 mipsn32el nios2 or1k ppc ppc64 ppc64abi32 ppc64le riscv32 riscv64 s390x sh4 sh4eb sparc sparc32plus sparc64 tilegx xtensa xtensaeb"  emerge   --autounmask-continue --autounmask=y --autounmask-write  -vn -j$JOBS qemu
 # QEMU_SOFTMMU_TARGETS=-x86_64
 
 mkdir -p /cross/crossdev/{profiles,metadata}
