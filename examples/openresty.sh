107beb0bf2263a40c355b92cf3beb74708e97402
wind <liyong@alot.pw>
2023-07-11 23:40:05 +0800

    [push]upgrade gentoo to latest,add riscv64 and loongarch64

--- examples/openresty.sh
+++ examples/openresty.sh
@@ -16,15 +16,16 @@ tar xvf openresty-1.17.8.2.tar.gz
 #correct mips64el setting for openssl
 sed -i "s/linux-mips64/linux64-mips64/g" openssl-1.1.1h/config
 
+PREFIX=/opt/openresty.$(arch)
 # compile it
 cd openresty-1.17.8.2
-time ./configure --prefix=/opt/openresty --with-http_v2_module --with-http_ssl_module --with-http_auth_request_module --with-pcre=$PWD/../pcre-8.44 --with-openssl=$PWD/../openssl-1.1.1h --with-zlib=$PWD/../zlib-1.2.11  --add-module=$PWD/../ngx-fancyindex-0.4.2
+time ./configure --prefix=$PREFIX --with-http_v2_module --with-http_ssl_module --with-http_auth_request_module --with-pcre=$PWD/../pcre-8.44 --with-openssl=$PWD/../openssl-1.1.1h --with-zlib=$PWD/../zlib-1.2.11  --add-module=$PWD/../ngx-fancyindex-0.4.2
 time make -j 8
 make install
 
-sudo mkdir /opt/openresty/nginx/conf.d
+sudo mkdir $PREFIX/nginx/conf.d
 # start nginx
-/opt/openresty/nginx/sbin/nginx
+$PREFIX/nginx/sbin/nginx
 #stop
 #pkill nginx
 
