9f317d8c0d512dece1ee70fa94d1d286c644f82d
Wind Li <liyong@alot.pw>
2023-07-19 23:57:38 +0800

    [push]try support ,mergeusr and cross build crossenv itself with crossany

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,8 @@
+ARG GCC_VERSION=
+ARG GLIBC_VERSION=
+ARG KERNEL_VERSION=
+ARG BINUTILS_VERSION=
+ARG BINUTILS_STAGE3_VERSION=
 # name the portage image
 FROM gentoo/portage:20230703 as portage
 
@@ -11,6 +16,11 @@ ADD . /cross/localrepo
 
 ENV FEATURES="-ipc-sandbox -network-sandbox -pid-sandbox -sandbox -usersandbox"
 # prepare base env
-RUN /bin/bash /cross/localrepo/prepare.sh
+RUN /bin/env GCC_VERSION="${GCC_VERSION}" \
+	GLIBC_VERSION="${GLIBC_VERSION}" \
+	KERNEL_VERSION="${KERNEL_VERSION}" \
+	BINUTILS_VERSION="${BINUTILS_VERSION}" \
+	BINUTILS_STAGE3_VERSION="${BINUTILS_STAGE3_VERSION}" \
+	/bin/bash /cross/localrepo/prepare.sh
 
 ENTRYPOINT [ "/crossenv" ]
--- prepare.sh
+++ prepare.sh
@@ -8,11 +8,11 @@ if [ "$JOBS" = 1 ]; then
     JOBS=2
 fi
 pushd $(dirname $0)
-cp -avf profiles/thirdpartymirrors /var/db/repos/gentoo/profiles/thirdpartymirrors
+cp -avf profiles/thirdpartymirrors $ROOT/var/db/repos/gentoo/profiles/thirdpartymirrors
 if [ "$USE_MIRROR" = "CN" ]; then
-  sed -i "s/rsync.gentoo.org/mirrors.tuna.tsinghua.edu.cn/g" /usr/share/portage/config/repos.conf
-  grep ^GENTOO_MIRRORS /etc/portage/make.conf>/dev/null 2>/dev/null || echo GENTOO_MIRRORS="https://mirrors.tuna.tsinghua.edu.cn/gentoo" >> /etc/portage/make.conf
-  grep ^gnu /etc/portage/mirrors >/dev/null 2>/dev/null|| echo gnu https://mirrors.tuna.tsinghua.edu.cn/gnu >> /etc/portage/mirrors
+  sed -i "s/rsync.gentoo.org/mirrors.tuna.tsinghua.edu.cn/g" $ROOT/usr/share/portage/config/repos.conf
+  grep ^GENTOO_MIRRORS $ROOT/etc/portage/make.conf>/dev/null 2>/dev/null || echo GENTOO_MIRRORS="https://mirrors.tuna.tsinghua.edu.cn/gentoo" >> $ROOT/etc/portage/make.conf
+  grep ^gnu $ROOT/etc/portage/mirrors >/dev/null 2>/dev/null|| echo gnu https://mirrors.tuna.tsinghua.edu.cn/gnu >> $ROOT/etc/portage/mirrors
 fi
 #emerge --tree --sync
 ulimit -a
@@ -20,18 +20,18 @@ ulimit -a
 emerge --tree -j$JOBS -vn --autounmask-continue --autounmask=y --autounmask-write dev-util/ninja lsof =sys-devel/make-4.3-r1
 #emerge --tree -j$JOBS -vn --autounmask-continue --autounmask=y --autounmask-write '=sys-devel/gcc-13*'
 #gcc-config 2
-emerge --tree -j$JOBS -vn --autounmask-continue --autounmask=y --autounmask-write crossdev vim dev-vcs/git dev-util/patchelf app-portage/gentoolkit dev-util/pkgdev sudo file app-admin/eselect app-arch/zip app-arch/zstd
-USE="static-user" QEMU_USER_TARGETS="x86_64 loongarch64 hexagon aarch64 aarch64_be alpha arm armeb cris hppa i386 m68k microblaze microblazeel mips mips64 mips64el mipsel mipsn32 mipsn32el nios2 or1k ppc ppc64 ppc64abi32 ppc64le riscv32 riscv64 s390x sh4 sh4eb sparc sparc32plus sparc64 tilegx xtensa xtensaeb"  emerge --tree   --autounmask-continue --autounmask=y --autounmask-write  -vn -j$JOBS '>app-emulation/qemu-8'
+PYTHON_TARGETS="python3_11" USE=pam emerge --tree -j$JOBS -vn --autounmask-continue --autounmask=y --autounmask-write crossdev vim dev-vcs/git dev-util/patchelf app-portage/gentoolkit dev-util/pkgdev sudo file app-admin/eselect app-arch/zip app-arch/zstd  dev-libs/libxml2
+PYTHON_TARGETS="python3_11" USE="static-user -python" QEMU_USER_TARGETS="x86_64 loongarch64 hexagon aarch64 aarch64_be alpha arm armeb cris hppa i386 m68k microblaze microblazeel mips mips64 mips64el mipsel mipsn32 mipsn32el nios2 or1k ppc ppc64 ppc64abi32 ppc64le riscv32 riscv64 s390x sh4 sh4eb sparc sparc32plus sparc64 tilegx xtensa xtensaeb"  emerge --tree   --autounmask-continue --autounmask=y --autounmask-write  -vn -j$JOBS '>app-emulation/qemu-8'
 # QEMU_SOFTMMU_TARGETS=-x86_64
 
-mkdir -p /cross/crossdev/{profiles,metadata}
-echo 'crossdev' > /cross/crossdev/profiles/repo_name
-echo 'masters = gentoo' > /cross/crossdev/metadata/layout.conf
-chown -R portage:portage /cross 
+mkdir -p $ROOT/cross/crossdev/{profiles,metadata}
+echo 'crossdev' > $ROOT/cross/crossdev/profiles/repo_name
+echo 'masters = gentoo' > $ROOT/cross/crossdev/metadata/layout.conf
+chown -R portage:portage $ROOT/cross 
 
-mkdir -p /etc/portage/repos.conf
-if [ ! -e /etc/portage/repos.conf/localrepo.conf ]; then
-cat > /etc/portage/repos.conf/localrepo.conf <<EOF
+mkdir -p $ROOT/etc/portage/repos.conf
+if [ ! -e $ROOT/etc/portage/repos.conf/localrepo.conf ]; then
+cat > $ROOT/etc/portage/repos.conf/localrepo.conf <<EOF
 [localrepo]
 location = $basefolder
 masters = gentoo
@@ -40,8 +40,8 @@ sync-type = git
 sync-uri = https://github.com/cross-any/cross-any.git
 EOF
 fi
-if [ ! -e /etc/portage/repos.conf/crossdev.conf ]; then
-cat > /etc/portage/repos.conf/crossdev.conf <<EOF
+if [ ! -e $ROOT/etc/portage/repos.conf/crossdev.conf ]; then
+cat > $ROOT/etc/portage/repos.conf/crossdev.conf <<EOF
 [crossdev]
 location = $crossrepofolder
 priority = 10
@@ -55,7 +55,7 @@ fi
 # else
 #   emerge --tree -vn -j$JOBS  '=net-libs/nodejs-14*::gentoo'
 # fi
-emerge --tree -vn -j$JOBS --autounmask-continue --autounmask=y --autounmask-write '=net-libs/nodejs-18.16.1'
+emerge --tree -vn -j$JOBS --autounmask-continue --autounmask=y --autounmask-write '=net-libs/nodejs-18.16.1' '<sys-devel/gcc-13'
 emerge --tree -vn -j$JOBS --autounmask-continue --autounmask=y --autounmask-write libffi '=dev-lang/python-3.11*' acct-group/nobody
 
 cat <<EOF >>/etc/locale.gen
@@ -65,15 +65,16 @@ EOF
 locale-gen
 popd
 #fpm to allow build rpm or deb package
-emerge --tree -j$JOBS -vn --autounmask-continue --autounmask=y --autounmask-write ruby rpm dpkg
+PYTHON_TARGETS="python3_11" LUA_SINGLE_TARGET=lua5-4 emerge --tree -j$JOBS -vn --autounmask-continue --autounmask=y --autounmask-write '<ruby-3.2'
+PYTHON_TARGETS="python3_11" LUA_SINGLE_TARGET=lua5-4 emerge --tree -j$JOBS -vn --autounmask-continue --autounmask=y --autounmask-write rpm dpkg
 gem install fpm
-find /usr/local/*/ruby/gems/*/gems/fpm-*/templates/ -name "*.sh" -o -name "*.sh.erb"|xargs sed -i "s#/bin/sh#/bin/bash#g"
-rm -rf /var/tmp/* /var/log/* /var/cache/*/*
-rm -rf /cross/localrepo/*.core
-ln -s $basefolder/register.sh /register
-ln -s $basefolder/register.sh /usr/bin/register
-ln -s $basefolder/crossenv /crossenv
-ln -s $basefolder/crossenv /usr/bin/crossenv
-ln -s $basefolder/crossit /usr/bin/crossit
-ln -s $basefolder/nativerun /usr/bin/nativerun
+find $ROOT/usr/local/*/ruby/gems/*/gems/fpm-*/templates/ -name "*.sh" -o -name "*.sh.erb"|xargs sed -i "s#/bin/sh#/bin/bash#g"
+rm -rf $ROOT/var/tmp/* $ROOT/var/log/* $ROOT/var/cache/*/*
+rm -rf $ROOT/cross/localrepo/*.core
+ln -s $basefolder/register.sh $ROOT/register
+ln -s $basefolder/register.sh $ROOT/usr/bin/register
+ln -s $basefolder/crossenv $ROOT/crossenv
+ln -s $basefolder/crossenv $ROOT/usr/bin/crossenv
+ln -s $basefolder/crossit $ROOT/usr/bin/crossit
+ln -s $basefolder/nativerun $ROOT/usr/bin/nativerun
 chmod +x $basefolder/crossenv
