7ef17a57405e061be66899259bf13877379ce81c
Wind Li <liyong@alot.pw>
2023-07-23 16:25:00 +0800

    [arch-only][arm64]run push arm64 in a new job

--- prepare.sh
+++ prepare.sh
@@ -68,8 +68,9 @@ en_US.UTF8 UTF-8
 EOF
 locale-gen
 popd
+ROOT= (PYTHON_TARGETS="python3_11" LUA_SINGLE_TARGET=lua5-4 emerge --tree -j$JOBS -vn --autounmask-continue --autounmask=y --autounmask-write rpm dpkg
 #fpm to allow build rpm or deb package
-(PYTHON_TARGETS="python3_11" LUA_SINGLE_TARGET=lua5-4 emerge --tree -j$JOBS -vn --autounmask-continue --autounmask=y --autounmask-write '<ruby-3.2' rpm dpkg \
+(USE="-rdoc" PYTHON_TARGETS="python3_11" LUA_SINGLE_TARGET=lua5-4 emerge --tree -j$JOBS -vn --autounmask-continue --autounmask=y --autounmask-write '<ruby-3.2' rpm dpkg \
 	&& $ROOT/usr/bin/gem install --install-dir $ROOT$(gem environment home) --bindir $ROOT/usr/local/bin fpm \
 	&& (find $ROOT/usr/local/*/ruby/gems/*/gems/fpm-*/templates/ -name "*.sh" -o -name "*.sh.erb"|xargs sed -i "s#/bin/sh#/bin/bash#g")) || echo please install later
 rm -rf $ROOT/var/tmp/* $ROOT/var/log/* $ROOT/var/cache/*/*
